119a747cd758a83872ed9e4b3e84a65db40c4fea
sakai-aigga <sajinghimire2010@gmail.com>
2026-02-03 15:08:08 +0545

    Created Create Course page

---
+++ src/pages/CreateCourse.jsx
@@ -0,0 +1,37 @@
+import Sidebar from "../components/Sidebar";
+import Input from "../components/Input";
+
+export default function CreateCourse() {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+      {/* Main content */}
+      <div className="flex-1 p-10 flex">
+        <div className="w-full max-w-full bg-white p-8 rounded-xl shadow-md">
+            
+          {/* Title */}
+          <h2 className="text-2xl font-bold text-gray-800 mb-6">
+            New Course
+          </h2>
+
+          {/* Form */}
+          <div className="space-y-5">
+            <Input placeholder="Course Name" />
+            <Input placeholder="Course Code" />
+
+            <textarea
+              placeholder="Course Description"
+              rows="4"
+              className="w-full border rounded-lg p-3 outline-none"
+            />
+
+            <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-lg shadow">
+              Create Course
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
