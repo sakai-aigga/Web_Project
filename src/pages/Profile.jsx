92dce1a7a2596bb01bd86ee89ee477c3af7291a7
sakai-aigga <sajinghimire2010@gmail.com>
2026-02-03 17:42:44 +0545

    Tweaked User information display and input fields,

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -13,20 +13,26 @@ export default function ShowProfile () {
                 <br />
                 <h1 className="font-semibold pb-3">You can edit your profile information here.</h1>
                 {/* Edit Profile form */}
-                <div className="p-4 max-w-lvw bg-gray-50 rounded-lg">
-                    Your current Username:
-                    <Input icon={<User />} placeholder={"Enter new name"} />
-                    <br />
-                    Your current Email:
-                    <Input icon={<Mail />} placeholder={"Enter new email"} />
-                    <button
-                    className="w-50 mt-4 bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl shadow text-center font-medium">
-                        Save Changes
-                    </button>
-                    <button
-                    className="ml-3 w-50 mt-4 bg-gray-300 hover:bg-gray-400 text-black py-3 rounded-xl shadow text-center font-medium">
-                        Cancel
-                    </button>
+                <div className="flex flex-col px-6 pb-6 max-w-lvw bg-gray-50 rounded-lg">
+                    <div className="pt-4">
+                        <p>Your current Username:</p>
+                        <p>Your current Email:</p>
+                    </div>
+                    <div className="flex flex-col pt-4 gap-4">
+                        <Input icon={<User />} placeholder={"Enter new name"} />
+                        <Input icon={<Mail />} placeholder={"Enter new email"} />
+                    </div>
+                    <div>
+                        <button
+                        className="w-50 mt-4 bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-xl shadow text-center font-medium">
+                            Save Changes
+                        </button>
+                        <button
+                        className="ml-3 w-50 mt-4 bg-gray-300 hover:bg-gray-400 text-black py-3 rounded-xl shadow text-center font-medium">
+                            Cancel
+                        </button>
+                    </div>
+                    
                 </div>
                 
               </div>
